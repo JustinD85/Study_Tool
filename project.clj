963da9f5650e79293009e3907528e90d0cbd5acf
Ubuntu <ubuntu@ip-172-31-0-40.us-east-2.compute.internal>
2019-01-13 05:51:32 +0000

    Add ability to send JSON back to requestor

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject base "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A backend for a studying tool"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -8,7 +8,9 @@
                  [mysql/mysql-connector-java "8.0.13"]
                  [ring "1.7.1"]
                  [compojure "1.6.1"]
-                 [http-kit "2.2.0"]]
+                 [http-kit "2.2.0"]
+                 [cheshire "5.8.1"]]
+  :plugins [[lein-auto "0.1.3"]]
   :main ^:skip-aot base.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
